c79a149b107cf27f3f4b88fb3380ff0e1b581192
Nishant Jha <168707776+Nishant7621@users.noreply.github.com>
2025-09-17 15:19:13 +0530

    Add files via upload

---
+++ conditional_statement.js
@@ -0,0 +1,122 @@
+// let a=5;
+// let b=3;
+// console.log('a=',a,'&b=',b);
+// a++ ;
+// console.log(a);
+
+
+
+// let a=5;
+// let b=3;
+// console.log('a=',a,'&b=',b);
+// console.log("++a=",++a);            //preincrement
+
+
+
+// let a=5;
+// let b=3;
+// console.log('a=',a,'&b=',b);
+
+// console.log(a++);
+// console.log(a);                    //postincrement
+
+
+
+// let a=5;
+// let b=3;
+// console.log('a=',a,'&b=',b);
+
+// console.log(a--);
+// console.log(a);
+
+
+
+// let a=5;
+// let b=3;
+// console.log('a=',a,'&b=',b);
+
+// console.log(--a);
+// console.log(a);
+
+
+
+// let a=5;
+// let b='5';  //convert string into number to check
+// console.log('a==b',a==b);  //true print hoga
+
+
+//strict version ===
+// let a=5;
+// let b='5';
+// console.log('a===b',a===b) //false print hoga
+
+
+
+
+// let a=5;
+// let b=6;
+// let cond1=a<b;
+// let cond2=a==b;
+// console.log("cond1 && cond2 =",cond1 && cond2);
+
+
+
+
+// let a=5;
+// let b=6;
+// let cond1=a<b;
+// let cond2=a==b;
+// console.log("cond1 && cond2 =",cond1 || cond2);
+
+
+
+// let a=5;
+// let b=6;
+// console.log("!(6<5) =",!(a>b));    //not equal to operater always return opposite of that condition
+
+
+
+
+// let mode = "light";
+// let color;
+
+// if(mode == "dark")  {
+//     color = "black";
+// } else {
+//     color = "white";
+// }
+// console.log(color);
+
+
+
+// let num = 20;
+// if (num % 2 === 0)  {
+//     console.log(num,"is even");
+// }
+// else {
+//     console.log(num,"is odd");
+// }
+
+
+
+
+// let mode = "silver";
+// let color;
+// if(mode == "dark") {
+//     color = "black";
+// } else if (mode == "blue")  {
+//     color = "blue";
+// } else if(mode == "pink")  {
+//     color = "pink";
+// } else {
+//     color = "white";
+// }
+// console.log(color);
+
+
+
+
+
+let age = 22;
+let result = age >= 18 ? "adult": "not adult";
+console.log(result);
