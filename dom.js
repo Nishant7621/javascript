50778495a90b96c48aea376aa03efd62caa96f5a
Nishant Jha <168707776+Nishant7621@users.noreply.github.com>
2025-10-14 23:38:22 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,70 @@
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+// let headings = document.getElementsByClassName("heading-class");
+// console.dir(headings);
+// console.log(headings);
+
+
+
+// let parahs = document.getElementsByTagName("p");
+// console.dir(parahs);
+
+
+// let firstEl = document.querySelector("p");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("p");
+// console.dir(allEl);
+
+
+
+// let firstEl = document.querySelector("#myId");
+// console.dir(firstEl);
+
+
+// let firstEl = document.querySelector(".myclass");
+// console.dir(firstEl);
+
+// console.dir(document.body.firstChild);
+
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let para = document.querySelector("p");
+// // console.log(para.getAttribute("class"));
+
+// console.log(para.setAttribute("class","newclass"));
+
+
+// div.style.backgroundColor = "green";
+
+// div.style.fontSize = "26px";
+
+// div.style.visibility = "hidden";
+
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "click me";
+// console.log(newBtn);
+
+// // let div = document.querySelector("div");
+// // // div.append(newBtn);
+// // // div.before(newBtn);
+// // div.after(newBtn);
+
+// let p = document.querySelector("p")
+// p.after(newBtn);
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i> hi, im new</i>";
+
+document.querySelector("body").prepend(newHeading);
+
+// let para=document.querySelector("p");
+// para.remove()
+
+
